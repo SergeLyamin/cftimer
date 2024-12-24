3ae1222e8afbf2072701d52040691154d7ca6550
SergeNotes <serge145@mail.ru>
2024-12-24 09:39:53 +0500

    deploy update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
-cd /home/timer/timer.lyamin.org
+
+# Обновляем код из репозитория
 git pull
+
+# Устанавливаем зависимости
 npm install
-pm2 restart timer 
+
+# Создаем необходимые директории
+mkdir -p www/dist
+mkdir -p www/dist/img
+
+# Копируем ассеты
+cp -r www/img/* www/dist/img/
+
+# Собираем CSS
+npm run css
+
+# Перезапускаем сервер (предполагая, что вы используете pm2)
+pm2 restart timer  # замените 'timer' на имя вашего процесса в pm2
